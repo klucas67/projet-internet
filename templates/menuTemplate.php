81bb1798f76f25d0736a1dbb720e5faf149e2a79
Clément David <Clment David>
2017-03-22 15:39:26 +0100

    ajout de css

--- templates/menuTemplate.php
+++ templates/menuTemplate.php
@@ -1,8 +1,13 @@
 
 
 <menu> 
-<a href="index.php?action=inscription ">INSCRIPTION </a>
-<p> </p>
-<a href=" index.php?action=connexion">CONNEXION</a>
+<div class="col-xs-3">
+    <div class="list-group">
+            <a class="list-group-item" href="index.php?action=inscription ">INSCRIPTION</a>
+            <a class="list-group-item" href="index.php?action=connexion"">CONNEXION</a>
+    </div>
+</div>
+
 </menu>
+
 
--- templates/userMenuTemplate.php
+++ templates/userMenuTemplate.php
@@ -6,7 +6,7 @@
 <li><a href="index.php?action=parties">Afficher toutes les parties</a></li>
 <li><a href="index.php?action=partiesencours">Parties en cours</a></li>
 <li><a href="index.php?action=partiesjoignables">Parties joignables</a></li>
-<li><a href="index.php">Deconnexion</a></li>
+<li><a href="index.php">Déconnexion</a></li>
 </ul>
 
 </menu>
