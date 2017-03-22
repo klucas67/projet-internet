a12655bd1181c22d4cc350ec992d687e74ad0075
Lucas Koenig <lucas.koenig1.@ens.etsmtl.ca>
2017-03-22 14:09:12 +0100

    connexion ok

--- model/User.class.php
+++ model/User.class.php
@@ -2,30 +2,25 @@
 
 class User extends Model {
 	protected static $table_name = 'joueur';
-	private $login;
-	private $password;
-	private $email;
-	private $nb_parties_jouees;
-	private $nb_parties_gagnees;
-	private $score_cumule;
+
 	
 	public function getLogin(){
-		return $this->login;
+		return $this->PSEUDO;
 	}
 	
-	public function __construct($log, $pwd, $mail){
-		$this->login = $log;
-		$this->password = $pwd;
-		$this->email = $mail;
-		$this->nb_parties_jouees = 0;
-		$this->nb_parties_gagnees = 0;
-		$this->score_cumule = 0;
-		
-	}
+	
+	
 	
 	public static function create($login, $pwd, $mail){
 	$sth = User::query("INSERT INTO joueur (PSEUDO, MOT_DE_PASSE, ADRESSE_MAIL, NB_PARTIES_JOUEES, NB_PARTIES_GAGNEES, SCORE_CUMULE) VALUES ('".$login."', '".$pwd."', '".$mail."', 0, 0, 0)");
-	$user = new User($login, $pwd, $mail); 
+	$user = new User(); 
+	$user->login = $login;
+		$user->password = $pwd;
+		$user->email = $mail;
+		$user->nb_parties_jouees = 0;
+		$user->nb_parties_gagnees = 0;
+		$user->score_cumule = 0;
+		
 	return $user;
 	}
 	public static function isLoginUsed($login){
@@ -38,8 +33,8 @@ class User extends Model {
 	public static function check($login, $pwd){
 	$sql = "SELECT * FROM joueur WHERE PSEUDO ='". $login . "' AND MOT_DE_PASSE = '".$pwd . "'";
 	$sth = User::query($sql);
-	
-	return 0;
+	$res = $sth->fetch();
+	return $res;
 	}
 	
 }
