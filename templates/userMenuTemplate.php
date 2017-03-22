568e35006428ba326ae165bd1963ea1025a5beb7
Lucas Koenig <lucas.koenig1.@ens.etsmtl.ca>
2017-03-22 17:30:34 +0100

    virer la merde

--- templates/userMenuTemplate.php
+++ templates/userMenuTemplate.php
@@ -1,7 +1,7 @@
 
 
 <menu> 
-<<<<<<< HEAD
+
 
 <div class="navbar navbar-default" role="navigation">
     <!-- Partie de la barre toujours affichée -->
@@ -18,12 +18,9 @@
     <!-- Partie de la barre masquée si la surface d'affichage est insuffisante -->
     <div class="collapse navbar-collapse" id="navbar-collapse-target">
         <ul class="nav navbar-nav">
-<?php
-=======
-<ul id="menu_horizontal">
+
 <?php
 
->>>>>>> refs/remotes/origin/master
 echo '<li><a href="index.php?action=creerpartie&user='. $args['user'].'&controller='. $args['controller']->getRequest()->getController(). '" >Créer une partie</a></li>';
 ?>
 <li><a href="index.php?action=partiesencours">Parties en cours</a></li>
